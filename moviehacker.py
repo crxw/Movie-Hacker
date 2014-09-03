ccfb745cfff94b0046b9f969e584bb7d31b742e6
Dale E. Moore <DaleEMoore@gMail.Com>
2014-09-03 13:51:07 -0500

    some more TODOs

---
+++ moviehacker.py
@@ -0,0 +1,4 @@
+__author__ = 'caleb'
+# TODO; maybe create a working linux terminal with animations, images, and sound to mimic the appearance of movie hacking.
+# TODO; accept user typing
+# TODO; fire off some activity, screen display, movie clip view, on user typing
