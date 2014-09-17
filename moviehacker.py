3ba3b64ef13ec8729bcfa0f30bf4e68e281335e2
Dale E. Moore <DaleEMoore@gMail.Com>
2014-09-17 14:54:52 -0500

    Using pygame, needs to be installed. I installed pygameoflife which installed pygame.

--- moviehacker.py
+++ moviehacker.py
@@ -2,3 +2,17 @@ __author__ = 'caleb'
 # TODO; maybe create a working linux terminal with animations, images, and sound to mimic the appearance of movie hacking.
 # TODO; accept user typing
 # TODO; fire off some activity, screen display, movie clip view, on user typing
+
+# From http://inventwithpython.com/pygame/chapter2.html
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((400, 300))
+pygame.display.set_caption('Hello World!')
+while True: # main game loop
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
